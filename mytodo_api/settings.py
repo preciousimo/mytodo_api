abeb04428665afd8af473c38366a2f532a30ce3a
preciousimo <preciousimoniakemu@gmail.com>
2022-11-04 22:51:47 +0100

    Install rest framework authtoken in settings

--- mytodo_api/settings.py
+++ mytodo_api/settings.py
@@ -39,9 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     
     'base',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
