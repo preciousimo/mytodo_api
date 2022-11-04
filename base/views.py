bda45f38d2ea525e0af09a0cc5b9588ce2dfac69
preciousimo <preciousimoniakemu@gmail.com>
2022-11-04 22:05:47 +0100

    create the TodoRetrieveUpdateDestroy generic view

--- base/views.py
+++ base/views.py
@@ -53,3 +53,11 @@ class TodoListCreate(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
