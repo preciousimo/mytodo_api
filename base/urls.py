c541b9e0e131360cbe84dd3f3e67a733979b300a
preciousimo <preciousimoniakemu@gmail.com>
2022-11-04 22:08:40 +0100

    Create path to complete a todo

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.TodoList.as_view()),
     path('create/', views.TodoListCreate.as_view()),
     path('todo/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
+    path('todos/<int:pk>/complete', views.TodoToggleComplete.as_view()),
 ]
