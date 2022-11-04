62c90798e13354e91bcac77e0990270ef0b30297
preciousimo <preciousimoniakemu@gmail.com>
2022-11-04 19:32:26 +0100

    customize creation of API process

--- base/views.py
+++ base/views.py
@@ -45,7 +45,10 @@ class TodoList(generics.ListAPIView):
 
 class TodoListCreate(generics.ListCreateAPIView):
     serializer_class = TodoSerializer
+
     def get_queryset(self):
         user = self.request.user
-        
         return Todo.objects.filter(user=user).order_by('-created')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
