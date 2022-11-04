9d65322feef03a1c332b4dd0c1e0017fc3816a58
preciousimo <preciousimoniakemu@gmail.com>
2022-11-04 23:00:23 +0100

    Implement the signup view

--- base/views.py
+++ base/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.db.models import Q
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework import generics, permissions
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
 
 from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 from .models import Todo
@@ -73,3 +78,20 @@ class TodoToggleComplete(generics.UpdateAPIView):
     def perform_update(self,serializer):
         serializer.instance.completed=not(serializer.instance.completed)
         serializer.save()
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request) # data is a dictionary
+            user = User.objects.create_user(
+                username=data['username'], password=data['password']
+            )
+            user.save()
+            
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token':str(token)},status=201)
+        except IntegrityError:
+            return JsonResponse(
+                {'error':'username taken. choose another username'}, status=400
+            )
