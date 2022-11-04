1a17c09e0943bb5921086be0f34ef1aa84bef79e
preciousimo <preciousimoniakemu@gmail.com>
2022-11-04 22:54:32 +0100

    Create part for the signup endpoint

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('create/', views.TodoListCreate.as_view()),
     path('todo/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
     path('todo/<int:pk>/complete', views.TodoToggleComplete.as_view()),
+
+    path('signup/', views.signup),
 ]
