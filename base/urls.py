e4f71fc5004f9df92cc3746086e4cbfe0c24d785
preciousimo <preciousimoniakemu@gmail.com>
2022-11-04 22:35:03 +0100

    Complete a Todo

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.TodoList.as_view()),
     path('create/', views.TodoListCreate.as_view()),
     path('todo/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
-    path('todos/<int:pk>/complete', views.TodoToggleComplete.as_view()),
+    path('todo/<int:pk>/complete', views.TodoToggleComplete.as_view()),
 ]
