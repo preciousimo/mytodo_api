b9e9b77853b39536efe8ecdaa42b007c0a986ace
preciousimo <preciousimoniakemu@gmail.com>
2022-11-04 22:07:24 +0100

    Change url name convention

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # path('', views.todo_list, name='todos'),
     path('', views.TodoList.as_view()),
     path('create/', views.TodoListCreate.as_view()),
-    path('todos/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
+    path('todo/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
 ]
